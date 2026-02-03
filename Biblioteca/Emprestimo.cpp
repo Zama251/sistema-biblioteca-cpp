9e230a26e94355de7e4cf72bcbace7691dbfe8ba
Cristhian Berci <crisberci0706@gmail.com>
2026-02-03 18:57:15 -0300

    Atualiza README

--- Biblioteca/Emprestimo.cpp
+++ Biblioteca/Emprestimo.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-    class Livro {
+int main () {};
+  class Livro {
 private:
     int id;
     string titulo;
@@ -31,20 +31,45 @@ public:
         return titulo;
     }
 };
+
 class Usuario {
 private:
     int id;
     string nome;
 
 public:
+        Usuario() {} // construtor vazio
     Usuario(int id, string nome) {
         this->id = id;
         this->nome = nome;
     }
 
-    string getNome() {
-        return nome;
+};
+class Emprestimo {
+private:
+    Usuario usuario;
+    Livro* livro;
+    string dataEmprestimo;
+    string dataDevolucao;
+    bool ativo;
+
+public:
+    Emprestimo(Usuario usuario, Livro* livro, string dataEmprestimo) {
+        this->usuario = usuario;
+        this->livro = livro;
+        this->dataEmprestimo = dataEmprestimo;
+        this->ativo = true;
+
+        livro->emprestar();
+    }
+
+    void devolver(string dataDevolucao) {
+        this->dataDevolucao = dataDevolucao;
+        this->ativo = false;
+        livro->devolver();
+    }
+
+    bool estaAtivo() {
+        return ativo;
     }
 };
-
-}
