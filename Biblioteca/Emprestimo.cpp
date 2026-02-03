70508a8e505f6a75d4717f9ca697775e11ea002a
Cristhian Berci <crisberci0706@gmail.com>
2026-02-03 18:04:18 -0300

    Atualiza README

--- Biblioteca/Emprestimo.cpp
+++ Biblioteca/Emprestimo.cpp
@@ -2,6 +2,35 @@
 using namespace std;
 
 int main (){
-    
+    class Livro {
+private:
+    int id;
+    string titulo;
+    bool disponivel;
+
+public:
+    Livro(int id, string titulo) {
+        this->id = id;
+        this->titulo = titulo;
+        this->disponivel = true;
+    }
+
+    bool estaDisponivel() {
+        return disponivel;
+    }
+
+    void emprestar() {
+        disponivel = false;
+    }
+
+    void devolver() {
+        disponivel = true;
+    }
+
+    string getTitulo() {
+        return titulo;
+    }
+};
+
     
 }
