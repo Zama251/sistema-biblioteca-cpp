368b80a828566e3fc2120a088d0bf961cf0fc14f
Zama251 <joaquimfranciscocr@gmail.com>
2026-02-02 23:24:29 -0300

    Logo começamos

--- Biblioteca/Main.cpp
+++ Biblioteca/Main.cpp
@@ -1,4 +1,6 @@
-Database db ("Biblioteca.db")
-db.executar("CREATE TABLE IF NOT EXISTS livros (...)");
-db.executar("CREATE TABLE IF NOT EXISTS usuarios (...)");
-db.executar("CREATE TABLE IF NOT EXISTS emprestimos (...)");
+#include <iostream>
+
+int main (){
+
+    
+}
---
+++ Database/Database.cpp
@@ -0,0 +1,4 @@
+Database db ("Biblioteca.db")
+db.executar("CREATE TABLE IF NOT EXISTS livros (...)");
+db.executar("CREATE TABLE IF NOT EXISTS usuarios (...)");
+db.executar("CREATE TABLE IF NOT EXISTS emprestimos (...)");
