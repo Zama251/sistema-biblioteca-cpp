d21c2aceff8c869d4d633ab423572440b7aaa431
Zama251 <joaquimfranciscocr@gmail.com>
2026-02-07 01:13:15 -0300

    Não sei como resolver

--- Biblioteca/Livros.cpp
+++ Biblioteca/Livros.cpp
@@ -4,14 +4,9 @@ using namespace std;
 
 struct Livros
 {
-    string Nome;
-    string Autor;
-    int Folhas;
-    int Ano_de_publicação;
-    int preço;
-    int Quantidade_Emprestimo;
-    int ISBN; //(International Standard Book Number)
- 
+    //Fisicos
+    //Digital
+    
     
    
-}
+};
