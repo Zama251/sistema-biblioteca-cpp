5891b5b8a90fea783952bed63be288569b380f18
Cristhian Berci <crisberci0706@gmail.com>
2026-02-07 01:52:14 -0300

    Atualiza README

--- Biblioteca/User.cpp
+++ Biblioteca/User.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <string> //Ele serve para você poder usar o tipo string do C++. Sem esse #include, o compilador não sabe o que é string.
+
 using namespace std;
 
-struct User{
-    string Nome; 
-    int CPF;
-    string Login; //public (Email)
-    int Senha; //protected? ou private
-    bool Status; //(Do emprestimo) (Se tem ou não livro emprestado?) + tempo restante?
+class User{ //se fosse struct tudo seria publico por padrão, ou seja, a senha e o cpf seriam publico, criando uma class ele fica mais seguro.
+   private: 
+   
+   string senha; //senha do usuario!! a senha como int é insegura e limitada, com string voce pode adicionar simbolos, tipos de letra e tamanho.
+   string cpf; // o cpf como int não daria certo por conta que ele tem 11 digitos e estoura o maximo do int.
 
-    // Aprincipio ta tudo errado aqui tenta pesquisar sobre por favor
+   public: 
+   
+   string login; //Email do usuario.
+   string nome;
+
+bool temEmprestimo; //o bool so serve pra true/false ele não pode gaurdar o tempo restante, se queremos usar ele tem que ser dessa forma.
+bool diasRestantes;
 };
