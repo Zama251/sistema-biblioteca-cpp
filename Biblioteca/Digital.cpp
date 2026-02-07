007871a422b80a56a3dd051ab7e7cf2261d49916
Zama251 <joaquimfranciscocr@gmail.com>
2026-02-07 01:14:25 -0300

    linha 7 acha necessario?

--- Biblioteca/Digital.cpp
+++ Biblioteca/Digital.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main (){
+struct LivroD{
 
-    
-}
+    string Nome;
+    string Autor;
+    int Tamanho; // KB, MB ou GB + tempo estimado de leitura
+    int Ano_Publicação;
+    int Preço; // Em R$
+    int Quantidade_downloads;
+
+};
