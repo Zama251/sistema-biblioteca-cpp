b6eafce28d499527de3d255c0c07589521f53daf
Zama251 <joaquimfranciscocr@gmail.com>
2026-02-23 20:36:26 -0300

    versão 0.2

--- Biblioteca/User.cpp
+++ Biblioteca/User.cpp
@@ -14,25 +14,25 @@ class User{ //se fosse struct tudo seria publico por padrão, ou seja, a senha e
    string login; //Email do usuario.
    string nome; 
 
-   bool TE; //Tem Emprestimo //o bool so serve pra true/false ele não pode gaurdar o tempo restante, se queremos usar ele tem que ser dessa forma.
-   int DR; // Dias Restantes
+   bool temEmprestimo; //Tem Emprestimo //o bool so serve pra true/false ele não pode gaurdar o tempo restante, se queremos usar ele tem que ser dessa forma.
+   int diasRestantes;// Dias Restantes
 
    User(string nome, string login, string cpf, string senha) {
    
-      this -> nome = nome;
+      this -> nome = nome; //indicação de local o -> this (literalmente uma seta)
       this -> login = login; 
       this -> cpf = cpf;
       this -> senha = senha;
 
-      TE = false;
-      DR = 0;
+     temEmprestimo = false;
+      diasRestantes = 0;
 
    }
 
-      bool VS (string tentativas); //Validador de senhas
-      bool VCPF (); //Validador de CPF
-      void FE (int D); // FazerEmprestimos | Dias
-      void DL (); //Devolver Livros
+      bool validarSenhas (const string& tentativas); //Validador de senhas (Explicação da const str...) Referência protegida (Não cria copia, Não pode modificar, Mais eficaz);
+      bool validarCpf (); //Validador de CPF
+      void fazerEmprestimos (int Dias); // FazerEmprestimos | Dias
+      void devolverLivros (); //Devolver Livros
 
       
 
