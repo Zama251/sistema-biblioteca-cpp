6cef0d80a6e3a8f15e63c7dddb78489992039d57
Zama251 <joaquimfranciscocr@gmail.com>
2026-02-26 19:04:28 -0300

    versão 0.3

--- Biblioteca/User.h
+++ Biblioteca/User.h
@@ -5,10 +5,21 @@
 
 class User{
 private:
+    std::string senha;
+    std::string cpf;
     
 public:
 
+    std::string login;
+    std::string nome;
+    bool temEmprestimo; //Tem Emprestimo //o bool so serve pra true/false ele não pode gaurdar o tempo restante, se queremos usar ele tem que ser dessa forma.
+    int diasRestantes;// Dias Restantes
 
+    User(std::string nome, std::string login, std::string cpf, std::string senha);
 
-
+    bool validarSenhas (const std::string& tentativas); //Validador de senhas (Explicação da const str...) Referência protegida (Não cria copia, Não pode modificar, Mais eficaz);
+    bool validarCpf (); //Validador de CPF
+    void fazerEmprestimos (int Dias); // FazerEmprestimos | Dias
+    void devolverLivros (); //Devolver Livros
+};
 #endif
